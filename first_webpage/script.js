534a79813cf4f3e5d049e458fbe67c835c02b82d
unknown <suleimanmadani@gmail.com>
2020-03-21 13:51:22 +0100

    javascript task 1

---
+++ first_webpage/script.js
@@ -0,0 +1,26 @@
+// name value
+let name = 'Suleiman Abdulshaqur';
+ 
+// store courses
+let courses = ['html', 'css', 'javascript', 'design'];
+
+// display name
+console.log(name);
+
+// display courses
+courses.forEach(course => console.log(course));
+
+
+// number of courses are even, so...
+
+for (i=1; i<=200; i++) {
+    if(i % 2 == 0) {
+        console.log(i);
+    }
+}
+
+
+
+
+
+
